3a34f8a43fa1070e529a6800a83a332f660bc672
chhotu24 <chhotushaw24@gmail.com>
2022-10-15 17:48:50 +0530

    done

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,7 +8,7 @@ router.get('/test-me', function (req, res) {
  console.log(xyz.greetings())
 
 
-    res.send('My first ever api!', 'this is chhotu shaw')
+    res.send('My first ever api!')
 
     //To be tried what happens if we send multiple response
     //res.send('My second api!')
