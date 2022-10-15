a200b8f73ac25985bc560dd6e0f0ace87b6b7a95
chhotu24 <chhotushaw24@gmail.com>
2022-10-16 00:48:05 +0530

    superb

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const underscore = require('underscore');
 const lodash= require('lodash');
-
+const arr=["january", "feabuary", "march", "april", "may", "june" , "july", "august", "september", "october", "november", "december"]
 
 
 const xyz = require('../logger');
@@ -14,6 +14,7 @@ router.get('/test-me', function (req, res) {
  console.log(xyz.greetings());
  console.log(xyz2.update());
  console.log(xyz3.mno(`     my name is chhotu shaw    `));
+ console.log(_.chunk(arr,3))
 
 
     res.send('My first ever api!')
