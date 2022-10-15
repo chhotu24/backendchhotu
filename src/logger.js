71defd50f6578625b6552a69277d2eba83311041
chhotu24 <chhotushaw24@gmail.com>
2022-10-15 17:50:50 +0530

    edit name

--- src/logger.js
+++ src/logger.js
@@ -1,5 +1,5 @@
 const welcome=function (){
-    return 'Welcome to my application. I am "+chhotu shaw+" and a part of FunctionUp lithium cohort.'
+    return 'Welcome to my application. I am chhotu shaw and,i am happy to be a part of FunctionUp lithium cohort.'
 }
 
 module.exports.greetings =welcome
