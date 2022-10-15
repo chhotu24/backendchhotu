dc8e899d974f8a01d656c451933fda6e42890744
chhotu24 <chhotushaw24@gmail.com>
2022-10-15 23:10:27 +0530

    final update

--- src/util/helper.js
+++ src/util/helper.js
@@ -3,7 +3,7 @@ const current_date=function(){
 const today=new Date();
 const day=today.getDay();
 const month=today.getMonth();
-const year=today.getUtcFullYear();
+const year=today.getFullYear();
 
 return "today is ${today} week number is ${day} month number is ${month} the year is ${year} . i am chhotu shaw , happy to be a part of lithium today we covered Node js module "
 
