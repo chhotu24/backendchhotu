f73c7eb3def34abf0bc4dd27ec7561cb63f612e6
chhotu24 <chhotushaw24@gmail.com>
2022-10-15 22:36:14 +0530

    2nd problem solution testing

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const underscore = require('underscore')
-const xyz = require('../logger')
+const underscore = require('underscore');
+const lodash= require('lodash');
+
+
+
+const xyz = require('../logger');
+const xyz2=require('../util/helper');
+const xyz3=require('../validator/formatter');
 
 
 router.get('/test-me', function (req, res) {
  console.log(xyz.greetings())
+ console.log(xyz2.date())
 
 
     res.send('My first ever api!')
---
+++ src/util/helper.js
@@ -0,0 +1,13 @@
+const current_date=function(){
+
+const today=new date();
+const day=current_date.getUtcDate();
+const month=current_date.getUtcMonth();
+const year=current_date.getUtcFullYear();
+
+return "today"
+
+}
+
+
+module.exports.date=current_date
