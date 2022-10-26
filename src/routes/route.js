f8b24876c28978184a64d86fd7688fe30b6d7ee3
chhotu24 <chhotushaw24@gmail.com>
2022-10-27 00:23:25 +0530

    done

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,14 +6,27 @@ const BookController= require("../controllers/bookController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
-})
+}
 
-router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
 
-router.post("/createBook", BookController.createBook  )
+router.post("/createBook", BookController.createBook )
 
-router.get("/getBooksData", BookController.getBooksData)
+router.get("/priceList", BookController.priceList)
+
+router.get("/booksInYear", BookController.booksInYear)
+
+router.get("/xINRBooks", BookController.xINRBooks)
+
+router.get("/particularBooks", BookController.particularBooks)
+
+router.get("/randomBooks", BookController.randomBooks)
+
+
+
+
+
+
+
 
 module.exports = router;
